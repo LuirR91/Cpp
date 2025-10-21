4033fef268f7403127ab7868685f334de590db90
Luis Ribeiro <luiribei@student.42lisboa.com>
2025-10-21 15:51:27 +0100

    cpp06 done

--- cpp06/ex02/src/main.cpp
+++ cpp06/ex02/src/main.cpp
@@ -17,18 +17,78 @@ Base* generate(void)
 			std::cout << "Generating object C" << std::endl;
 			return base = new C();
 	}
-	return base;
+	return NULL;
 }
+
 void identify(Base* p)
 {
+	if (dynamic_cast<A*>(p))
+		std::cout << "Type of the object pointed to by p: A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "Type of the object pointed to by p: B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "Type of the object pointed to by p: C" << std::endl;
 	return ;
 }
+
 void identify(Base& p)
 {
+	try
+	{
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "Type of the object pointed to by p: A" << std::endl;
+	}
+	catch(const std::exception& e){}
+
+	try
+	{
+		B &b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "Type of the object pointed to by p: B" << std::endl;
+	}
+	catch(const std::exception& e){}
+
+	try
+	{
+		C &c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "Type of the object pointed to by p: C" << std::endl;
+	}
+	catch(const std::exception& e){}
 	return ;
 }
 
 int main()
 {
+	Base	*test1;
+	Base	*test2;
+	Base	*test3;
+
+	srand (time(NULL));
+
+	test1 = generate();
+	test2 = generate();
+	test3 = generate();
+
+	std::cout << "\n<<<<< Base	*identify >>>>>" << std::endl;
+	std::cout << "Test 1: ";
+	identify(test1);
+	std::cout << "Test 2: ";
+	identify(test2);
+	std::cout << "Test 3: ";
+	identify(test3);
+
+	std::cout << "\n<<<<< Base& identify >>>>>" << std::endl;
+	std::cout << "Test 1: ";
+	identify(*test1);
+	std::cout << "Test 2: ";
+	identify(*test2);
+	std::cout << "Test 3: ";
+	identify(*test3);
+
+	delete test1;
+	delete test2;
+	delete test3;
 	return 0;
 }
